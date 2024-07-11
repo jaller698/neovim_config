833fa3f708703bf143c847ebf738d8e278d3fe45
jaller698 <c.soetorp@gmail.com>
2024-07-11 20:43:45 +0200

    second commit

--- lua/community.lua
+++ lua/community.lua
@@ -6,8 +6,7 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.rust"}
-  { import = "astrocommunity.pack.cpp"}
   -- import/override with your plugins folder
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.pack.rust"}
 }
