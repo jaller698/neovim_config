6ee6c42bb3794fb9247df5d7fca13452e276f63f
Christian Frølund Søtorp (CSSO) <csso@eposaudio.com>
2024-07-22 12:46:48 +0200

    call git update

--- init.lua
+++ init.lua
@@ -19,7 +19,7 @@ local function update_config()
         end
     end
 end
-
+update_config()
 -- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
 -- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
 local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
