3ce96a693191a7e45a54051ed040d1ffedf2a071
jaller698 <c.soetorp@gmail.com>
2024-10-27 12:28:04 +0100

    inital lazy setup

--- lua/neovide.lua
+++
@@ -1,6 +0,0 @@
- --- local configuartion only used when running from neovide
- ---
-
- vim.o.guifont = "EnvyCodeR_Nerd_Font:h12"
- vim.g.neovide_scale_factor = 1.0
- vim.g.neovide_cursor_animation_length = 0
