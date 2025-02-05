66a1e8f8bad0efd2552a2f61be4c750f9283ee53
jaller698 <c.soetorp@gmail.com>
2025-02-05 19:59:28 +0100

    init commit

--- lua/plugins/astroui.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "AstroNvim/astroui",
-  lazy = false, -- disable lazy loading
-  priority = 10000, -- load AstroUI first
-  opts = {
-    -- set configuration options  as described below
-  },
-}
